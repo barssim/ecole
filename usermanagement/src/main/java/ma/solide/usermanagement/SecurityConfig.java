f2ba21d037b97874beca7448e009c88be9a683b6
eqa <ahmed.elqarfaoui@iso-gruppe.de>
2026-07-10 12:46:53 +0200

    correct SecurityConfig

--- usermanagement/src/main/java/ma/solide/usermanagement/SecurityConfig.java
+++ usermanagement/src/main/java/ma/solide/usermanagement/SecurityConfig.java
@@ -1,5 +1,8 @@
 package ma.solide.usermanagement;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -7,13 +10,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/api/auth/**").permitAll()
-                .anyRequest().authenticated()
-			)
-		.formLogin(form -> form.disable())
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers("/api/auth/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(form -> form.disable())
                 .httpBasic(httpbasic -> httpbasic.disable());
 
         return http.build();
-
     }
+}
