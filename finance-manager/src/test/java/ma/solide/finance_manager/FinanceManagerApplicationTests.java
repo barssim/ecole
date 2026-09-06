3118e25d989ff294ea761d73d0e48886524d2866
barssim <qarfaoui@gmail.com>
2026-09-06 11:48:38 +0200

    kafka refactoring

--- finance-manager/src/test/java/ma/solide/finance_manager/FinanceManagerApplicationTests.java
+++ finance-manager/src/test/java/ma/solide/finance_manager/FinanceManagerApplicationTests.java
@@ -2,8 +2,10 @@ package ma.solide.finance_manager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FinanceManagerApplicationTests {
 
 	@Test
